ed69e333c37ee68c9418e942a1d4b9c586746779
ingens <67170142+ingensargens@users.noreply.github.com>
2023-09-30 12:16:53 -0500

    toggle dark mode by default

---
+++ static/styles/darkMode.js
@@ -0,0 +1,18 @@
+// Get the switch element
+const toggleSwitch = document.getElementById('toggleSwitch');
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    if (toggleSwitch.checked) {
+        // Enable dark mode
+        document.body.classList.add('dark-mode');
+    } else {
+        // Disable dark mode
+        document.body.classList.remove('dark-mode');
+    }
+}
+toggleSwitch.checked = true;
+
+toggleDarkMode();
+// Listen for the switch change event
+toggleSwitch.addEventListener('change', toggleDarkMode);
