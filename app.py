14ed09782ff93660e4b22c475aff54d8fabafdbf
ingens <67170142+ingensargens@users.noreply.github.com>
2023-10-01 18:03:52 -0500

    finalized layout.html, began lyrics

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 import subprocess
 import os
+from lyricsgenius import Genius
 
+
+#run setup cmds
 def setup():
 
     # Specify the path to your commands.txt file
@@ -18,22 +21,20 @@ def setup():
                 key, value = line[len("export "):].split("=", 1)
                 os.environ[key] = value
 setup()
+
+#genius
+genius = Genius(f'{os.environ.get("GENIUS_TOKEN")}')
+
 #spotify auth
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(redirect_uri='http://localhost:5000/', scope="user-top-read user-read-recently-played user-read-currently-playing user-library-read"))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(redirect_uri='http://localhost:5010/', scope="user-top-read user-read-recently-played user-read-currently-playing user-library-read"))
 
 # Configure application
 app = Flask(__name__)
 
-top_artist = 'Pierce the Veil'
-top_tracks = ["car", "HOTM", "circles"]
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-
-
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -42,10 +43,15 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
+#routes
 @app.route('/')
 def index():
-    # Debugging: Print the top_artist and top_tracks to the console
-    return render_template('layout.html', top_artist=top_artist, top_tracks=top_tracks)
+    return render_template('index.html')
+
+@app.route('/lyrics')
+def lyrics():
+    return render_template('lyrics.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
