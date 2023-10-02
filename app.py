9e2404d5824beee5ec841892abd2cec2b16ee25f
ingens <67170142+ingensargens@users.noreply.github.com>
2023-10-01 20:26:53 -0500

    added basic functionality for lyrics get and post

--- app.py
+++ app.py
@@ -49,9 +49,16 @@ def after_request(response):
 def index():
     return render_template('index.html')
 
-@app.route('/lyrics')
+@app.route('/lyrics', methods=["GET", "POST"])
 def lyrics():
-    return render_template('lyrics.html')
+    if request.method == "GET":
+        return render_template('lyricsSearch.html')
+    else: #method = post
+        song = request.form.get("song_query")
+        songLyrics = genius.search_song(title=song).lyrics
+        print(songLyrics)
+        return render_template('lyrics.html', lyrics=songLyrics)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5010)
