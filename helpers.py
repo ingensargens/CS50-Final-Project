092ca3e00b5713fe2f8a5dca055d8623d0dcab21
ingens <67170142+ingensargens@users.noreply.github.com>
2023-10-10 19:17:56 -0500

    not working drawOnImage

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import os
-def setup():
+from PIL import Image, ImageDraw, ImageFont
 
+def setup():
     # Specify the path to your commands.txt file
     commands_file_path = "commands.txt"
 
@@ -11,3 +12,27 @@ def setup():
             if line.startswith("export "):
                 key, value = line[len("export "):].split("=", 1)
                 os.environ[key] = value
+
+def draw_text_on_image(text):
+
+    image = Image.open("templates/marble.jpg")
+    draw = ImageDraw.Draw(image)
+
+    # Specify the font and size
+    font = ImageFont.load_default()
+    font_size = 36
+
+    # Specify the text and position
+    position = (50, 50)
+
+    # Specify the text color (R, G, B)
+    text_color = (255, 255, 255)
+
+    # Add the text to the image
+    draw.text(position, text, fill=text_color, font=font)
+
+    # Save the modified image
+    image.save("output_image.jpg")
+
+    # Close the image
+    image.close()
